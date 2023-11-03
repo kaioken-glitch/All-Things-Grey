2f55ecb481c12de5fefcf81fec5fe55158dbaf83
kaioken-glitch <83126699+kaioken-glitch@users.noreply.github.com>
2023-11-03 12:22:52 +0100

    Add files via upload

---
+++ global.js
@@ -0,0 +1,75 @@
+const h2Element - document
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function updateProgressBar(percentage) {
+    //var progressBar = document.getElementById("my-progress-bar");
+   // progressBar.style.width = percentage + "%";
+    //progressBar.innerHTML = percentage + "%";
+  //}//
+
+  // Example usage: updateProgressBar(50); // Updates the progress to 50%
+
+//document.addEventListener("DOMContentLoaded",   function simulateProgress() {
+   // var percentage = 0;
+    //var interval = setInterval(function () {
+     // percentage += 1;
+     // updateProgressBar(percentage);
+      //if (percentage >= 50) {
+       // clearInterval(interval);
+      //}//
+    //}, 50); // Adjust the interval duration for faster or slower progress
+  //});//
+  
+  // Call simulateProgress to see the progress bar in action
+ // simulateProgress();//
